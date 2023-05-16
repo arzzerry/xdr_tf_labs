09eb1a9c24579d4af6d916282a954acd6076b337
Alpha Serry [afs macbook] <alpha.serry@accenturefederal.com>
2023-05-16 11:27:39 -0400

    Add instance_sg.tf file and made corrections to the main.tf file

--- main.tf
+++ main.tf
@@ -2,6 +2,12 @@ resource "aws_instance" "example" {
   ami           = "ami-0578f2b35d0328762"
   instance_type = "t2.micro"
 
+  user_data                   = <<-EOF
+                #!/bin/bash
+                echo "Hello, World" > index.xhtml
+                nohup busybox httpd -f -p 8080 &
+                EOF
+  user_data_replace_on_change = true
   tags = {
     Name = "terraform-example"
   }
