7b136935cf0ca51bd62dab4b329400fd5fb904cf
Alpha Serry [afs macbook] <alpha.serry@accenturefederal.com>
2023-05-18 11:11:02 -0400

    Adds some new tf config files to the project code

--- instance_sg.tf
+++ instance_sg.tf
@@ -2,8 +2,8 @@ resource "aws_security_group" "instance_sg" {
   name        = "terraform-example-instance"
   description = "Allow http inbound traffic"
   ingress {
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = var.server_port
+    to_port     = var.server_port
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
--- main.tf
+++ main.tf
@@ -1,15 +1,18 @@
-resource "aws_instance" "example" {
-  ami                    = "ami-0578f2b35d0328762"
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.instance_sg.id]
+# This is the ASG launch configuraiton block
+resource "aws_launch_configuration" "example" {
+  image_id        = "ami-0578f2b35d0328762"
+  instance_type   = "t2.micro"
+  security_groups = [aws_security_group.instance_sg.id]
 
-  user_data                   = <<-EOF
+  user_data = <<-EOF
                 #!/bin/bash
                 echo "Hello, World" > index.xhtml
-                nohup busybox httpd -f -p 8080 &
+                nohup busybox httpd -f -p ${var.server_port} &
                 EOF
-  user_data_replace_on_change = true
-  tags = {
-    Name = "terraform-example"
+
+  # Required when using a launch configuration with an auto
+  # scaling group.
+  lifecycle {
+    create_before_destroy = true
   }
 }
